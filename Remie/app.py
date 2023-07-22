da6d1a16042d9b7829fc1b7ac7eb4ef8f2c6f42b
Khaylah <khaylahrscholar@gmail.com>
2023-07-21 20:09:54 -0400

    Popup for Dashboard

--- Remie/app.py
+++ Remie/app.py
@@ -4,13 +4,15 @@ app = Flask(__name__)
 
 #routes
 @app.route('/')
-@app.route('/sidebar')
-def sidebar():
-    return render_template('index.html',title='sidebar')
-
-# @app.route('/index')
-# def index():
-#     return render_template('index.html',title='index')
+@app.route('/base')
+def base():
+    return render_template('login.html',title='sidebar')
+@app.route('/basepopup')
+def basepopup():
+    return render_template('basepopup.html',title='sidebar')
+@app.route('/index')
+def index():
+    return render_template('index.html',title='index')
 
 # @app.route('/login')
 # def index():
--- Remie/static/js/nav.js
+++ Remie/static/js/nav.js
@@ -5,3 +5,22 @@ function sideClick(){
      $('#sidebar').toggleClass('active');
   });
 }
+
+let popupWindow;
+
+function openPopup() {
+  // Specify the URL of your website
+  const url = '/index'; // Replace '/index' with the actual URL of your website
+
+  // Specify the features of the popup window (width and height set directly)
+  const windowFeatures = 'width=593,height=711,resizable=no,scrollbars=no,status=no,toolbar=no';
+
+  // Open the popup window
+  popupWindow = window.open(url, '_blank', windowFeatures);
+
+  // Add an event listener for the 'resize' event on the opened window
+  popupWindow.addEventListener('resize', () => {
+    // Reset the window size to the original fixed size
+    popupWindow.resizeTo(593, 711);
+  });
+}
