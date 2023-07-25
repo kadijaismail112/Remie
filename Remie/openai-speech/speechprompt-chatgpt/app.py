c7772f75ead4319b26e0f9a7d4ad5e480f5518f1
Graciela Figueiras <gracielaifigueiras@gmail.com>
2023-07-25 19:56:22 +0000

    chat stufff

---
+++ Remie/openai-speech/speechprompt-chatgpt/app.py
@@ -0,0 +1,43 @@
+import openai
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+API_KEY = "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"
+openai.api_key = API_KEY
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api", methods=["POST"])
+def api():
+    # print("backend grab form", request.form)
+    # print("backend grab form per: ", request.form.get('username'), request.form.get('password'))
+
+    text_message = request.form.get('text_message')
+
+    response_message=chatgpt_process_query(text_message)
+
+    return jsonify(response_message)
+
+
+chat_log = []
+
+def chatgpt_process_query(message):
+    chat_log.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat_log
+    )
+    assistant_response = response['choices'] [0] ['message'] ['content']
+
+    clean_assistant_response=assistant_response.strip("\n").strip()
+    print("ChatGPT:", clean_assistant_response)
+    chat_log.append({"role": "assistant", "content": clean_assistant_response})
+
+    return clean_assistant_response 
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
---
+++ Remie/openai-speech/textprompt-chatgpt/app.py
@@ -0,0 +1,41 @@
+import openai
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+API_KEY = ""
+openai.api_key = API_KEY
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api", methods=["POST"])
+def api():
+
+    text_message = request.form.get('text_message')
+
+    response_message=chatgpt_process_query(text_message)
+
+    return jsonify(response_message)
+
+
+chat_log = []
+
+def chatgpt_process_query(message):
+    chat_log.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat_log
+    )
+    assistant_response = response['choices'] [0] ['message'] ['content']
+
+    clean_assistant_response=assistant_response.strip("\n").strip()
+    print("ChatGPT:", clean_assistant_response)
+    chat_log.append({"role": "assistant", "content": clean_assistant_response})
+
+    return clean_assistant_response 
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
