0c6d544a728d3cd2a155dc8f860aeece7e2dd55a
Kadija Ismail <kadijaismail112@gmail.com>
2023-07-25 18:29:52 -0400

    clean up codebase

--- server/app.py
+++ server/app.py
@@ -1,9 +1,15 @@
+<<<<<<< HEAD:server/app.py
 from flask import Flask, render_template, jsonify, request
 from model import chatgpt_process_query, classifier, chat_log
+=======
+import openai
+from flask import Flask, render_template,  jsonify, request
+>>>>>>> 2d042fa (clean up codebase):app.py
 
 app = Flask(__name__)
 
 #routes
+<<<<<<< HEAD:server/app.py
 @app.route('template/')
 
 @app.route('template/login')
@@ -22,32 +28,54 @@ def basepopup():
 def dash():
     return render_template('index.html',title='dashboard')
 
+=======
+@app.route('/')
+@app.route('/login')
+def login():
+    return render_template('login.html',title='sidebar')
+@app.route('/register')
+def register():
+    return render_template('register.html',title='register')
+@app.route('/basepopup')
+def basepopup():
+    return render_template('basepopup.html',title='sidebar')
+@app.route('/dashboard')
+def dash():
+    return render_template('index.html',title='dashboard')
+>>>>>>> 2d042fa (clean up codebase):app.py
 @app.route('/convos')
 def convo():
     return render_template('convo.html',title='convo')
 
+API_KEY = "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"
+openai.api_key = API_KEY
+
+@app.route("/api", methods=["POST"])
 def api():
-    # Get the message from the request
+    # print("backend grab form", request.form)
+    # print("backend grab form per: ", request.form.get('username'), request.form.get('password'))
+
     text_message = request.form.get('text_message')
-    message_response = chatgpt_process_query(classifier, text_message)
+    print("response:" + text_message)
+    response_message=chatgpt_process_query(text_message)
 
-    if message_response == "1":
-        chat_response = chatgpt_process_query(chat_log, text_message)
-        return jsonify(chat_response)
-    else:
-        print("Sorry :( I don't know how to do that yet.")
-    # elif message_response == "2":
-    #     print("2")
-    #     return jsonify({"Response": f"Creating invite: {text_message}"})
-    # elif message_response == "3":
-    #     print("3")
-    #     return jsonify({"Response": f"Sending email: {text_message}"})
-    # elif message_response == "4":
-    #     print("4")
-    #     return jsonify({"Response": f"Playing song: {text_message}"})
-    # else:
-    #     print("5")
-    #     return "didnt work :/"
+    return jsonify(response_message)
 
+
+chat_log = []
+
+def chatgpt_process_query(message):
+    chat_log.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat_log
+    )
+    assistant_response = response['choices'] [0] ['message'] ['content']
+
+    clean_assistant_response=assistant_response.strip("\n").strip()
+    print("ChatGPT:", clean_assistant_response)
+    chat_log.append({"role": "assistant", "content": clean_assistant_response})
+
+    return clean_assistant_response 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/navpop.js
+++ static/js/navpop.js
@@ -1,11 +1,17 @@
 
+// function sideClick() {
+//    $(document).ready(function () {
+//        $('#sidebar').toggleClass('active');
+//    });
+// }
 
-function sideClick() {
-   $(document).ready(function () {
-       $('#sidebar').toggleClass('active');
-   });
-}
+// function bot_response() {
+//    $(document).ready(function () {
+//        $('.bot_mouth').css('animation-name', 'mouthtalk');
+//    });
+// }
 
+<<<<<<< HEAD
 function bot_response() {
    $(document).ready(function () {
        $('.bot_mouth').css('animation-name', 'mouthtalk');
@@ -59,3 +65,52 @@ function bot_listen(button) {
        console.log("Error in async", error);
    });
 }
+=======
+// function bot_listen(button) {
+//    var player = document.getElementById("music");
+//    player.classList.toggle("paused");
+
+//    // Get the user input from the text field
+//    var userInput = document.querySelector("input[name=dash_input]").value;
+
+//    // Create a FormData object to send the user input to the backend
+//    var formData = new FormData();
+//    formData.append("text_message", userInput);
+
+//    // Send a POST request to the Chat GPT API endpoint
+//    fetch("/api", {
+//        method: "POST",
+//        body: formData
+//    })
+//    .then(function(response) {
+//        // Handle the response from the API
+//        if (response.ok) {
+//            return response.json();
+//        } else {
+//            throw new Error("Network response was not ok.");
+//        }
+//    })
+//    .then(function(data) {
+//        // Display the response from the Chat GPT API in the conversation list
+//        const conversationList = document.getElementById("conversation");
+//        const userMessageItem = document.createElement("li");
+//        userMessageItem.textContent = `User: ${userInput}`;
+//        conversationList.appendChild(userMessageItem);
+
+//        const assistantMessageItem = document.createElement("li");
+//        assistantMessageItem.textContent = `Assistant: ${data.assistant_response}`;
+//        conversationList.appendChild(assistantMessageItem);
+
+//        // Speak the assistant response using speech synthesis
+//        if ('speechSynthesis' in window) {
+//            let utterance = new SpeechSynthesisUtterance(data.assistant_response);
+//            speechSynthesis.speak(utterance);
+//        } else {
+//            console.log("speech synthesis isn't supported");
+//        }
+//    })
+//    .catch(function(error) {
+//        console.log("Error in async", error);
+//    });
+// }
+>>>>>>> 2d042fa (clean up codebase)
