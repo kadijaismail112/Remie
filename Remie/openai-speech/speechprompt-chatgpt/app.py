810021201539e1cdb47b521d57dd93f84e6112fb
Graciela Figueiras <gracielaifigueiras@gmail.com>
2023-07-25 21:09:58 +0000

    no se..

--- Remie/openai-speech/speechprompt-chatgpt/app.py
+++ Remie/openai-speech/speechprompt-chatgpt/app.py
@@ -1,10 +1,14 @@
 import openai
 from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes, allowing requests from any origin
 
-API_KEY = "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"
+API_KEY = "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"
 openai.api_key = API_KEY
+
+chat_log = []
 
 @app.route("/")
 def index():
@@ -12,17 +16,9 @@ def index():
 
 @app.route("/api", methods=["POST"])
 def api():
-    # print("backend grab form", request.form)
-    # print("backend grab form per: ", request.form.get('username'), request.form.get('password'))
-
     text_message = request.form.get('text_message')
-
-    response_message=chatgpt_process_query(text_message)
-
+    response_message = chatgpt_process_query(text_message)
     return jsonify(response_message)
-
-
-chat_log = []
 
 def chatgpt_process_query(message):
     chat_log.append({"role": "user", "content": message})
@@ -30,14 +26,11 @@ def chatgpt_process_query(message):
         model="gpt-3.5-turbo",
         messages=chat_log
     )
-    assistant_response = response['choices'] [0] ['message'] ['content']
-
-    clean_assistant_response=assistant_response.strip("\n").strip()
+    assistant_response = response['choices'][0]['message']['content']
+    clean_assistant_response = assistant_response.strip("\n").strip()
     print("ChatGPT:", clean_assistant_response)
     chat_log.append({"role": "assistant", "content": clean_assistant_response})
-
     return clean_assistant_response 
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=8000)
