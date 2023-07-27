645a908810b4802704c7f43ff9f10182e3acb91d
Kadija Ismail <kadijaismail112@gmail.com>
2023-07-27 02:04:32 -0400

    still trying to go back

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import openai
-import os
 from flask import Flask, render_template,  jsonify, request
-from model import chatgpt_process_query, agent, classifier, chat_log
 
 app = Flask(__name__)
 
@@ -23,30 +21,35 @@ def dash():
 def convo():
     return render_template('convo.html',title='convo')
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+API_KEY = "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"
+openai.api_key = API_KEY
 
 @app.route("/api", methods=["POST"])
 def api():
-    # Get the message from the request
+    # print("backend grab form", request.form)
+    # print("backend grab form per: ", request.form.get('username'), request.form.get('password'))
+
     text_message = request.form.get('text_message')
-    message_response = chatgpt_process_query(classifier, text_message)
-    print(type(message_response))
-    if message_response == "1":
-        chat_response = chatgpt_process_query(chat_log, text_message)
-        return jsonify(chat_response)
-    elif message_response == "2":
-        print("2")
-        return jsonify({"Response": f"Creating invite: {text_message}"})
-    elif message_response == "3":
-        print("3")
-        return jsonify({"Response": f"Sending email: {text_message}"})
-    elif message_response == "4":
-        print("4")
-        return jsonify({"Response": f"Playing song: {text_message}"})
-    else:
-        print("5")
-        return "didnt work :/"
+    print("response:" + text_message)
+    response_message=chatgpt_process_query(text_message)
+
+    return jsonify(response_message)
 
 
+chat_log = []
+
+def chatgpt_process_query(message):
+    chat_log.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat_log
+    )
+    assistant_response = response['choices'] [0] ['message'] ['content']
+
+    clean_assistant_response=assistant_response.strip("\n").strip()
+    print("ChatGPT:", clean_assistant_response)
+    chat_log.append({"role": "assistant", "content": clean_assistant_response})
+
+    return clean_assistant_response 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
